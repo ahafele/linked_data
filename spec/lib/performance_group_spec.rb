c4b084c2c103e2fab7e89cbe9c91734df69c9d09
Alissa T Hafele <ahafele@stanford.edu>
2016-06-07 14:30:29 -0700

    add a name spec

--- spec/lib/performance_group_spec.rb
+++ spec/lib/performance_group_spec.rb
@@ -6,6 +6,13 @@ describe 'a PerformanceGroup' do
     foo=PerformanceGroup.new
     expect(foo).to be_kind_of PerformanceGroup
   end
+  it 'has a name' do
+  	foo = PerformanceGroup.new
+  	foo.name = 'The Beatles'
+  	expect(foo.name).to eq 'The Beatles'
+
+  	puts foo.name 
+  end
 
 end
 
