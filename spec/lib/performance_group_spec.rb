e3d179624c9416fda19649b772e377a7968cbf21
Alissa T Hafele <ahafele@stanford.edu>
2016-06-07 14:48:33 -0700

    change to instance variable

--- spec/lib/performance_group_spec.rb
+++ spec/lib/performance_group_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper.rb'
 require 'PerformanceGroup'
 
-describe 'a PerformanceGroup' do 
+describe 'a PerformanceGroup' do
+	before :each do 
+		@foo = PerformanceGroup.new #instance veriable instead of local ie whole test sweet
+	end
   it 'can instantiate' do
-    foo=PerformanceGroup.new
-    expect(foo).to be_kind_of PerformanceGroup
+        expect(@foo).to be_kind_of PerformanceGroup
   end
   it 'has a name' do
-  	foo = PerformanceGroup.new
-  	foo.name = 'The Beatles'
-  	expect(foo.name).to eq 'The Beatles'
-  	puts foo.name 
+  	@foo.name = 'The Beatles'
+  	expect(@foo.name).to eq 'The Beatles'
+  	puts @foo.name 
   end
   it 'has a list of albums' do
-  	foo = PerformanceGroup.new
-  	albums = ["Hard Days Night", "White Alumb", "I don't know"]
-  	foo.albums = albums #sets array
+   	albums = ["Hard Days Night", "White Alumb", "I don't know"]
+  	@foo.albums = albums #sets array
   end
 
 end
